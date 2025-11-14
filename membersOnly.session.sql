5e458fe52c44770b9df501acb32a62b32fd522ed
Triston Land <tristonland@gmail.com>
2025-11-14 00:03:15 -0700

    schema

---
+++ membersOnly.session.sql
@@ -0,0 +1,27 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS users (
+  id          SERIAL PRIMARY KEY,
+  username    VARCHAR(36) UNIQUE NOT NULL,
+  password    VARCHAR(120) NOT NULL,
+  email       VARCHAR(320) UNIQUE,
+  first_name  VARCHAR(36) NOT NULL,
+  last_name   VARCHAR(36),
+  member      BOOLEAN NOT NULL DEFAULT false,
+  admin       BOOLEAN NOT NULL DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  id                SERIAL PRIMARY KEY,
+  author_id         INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  profile_id        INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  title             VARCHAR(280),
+  message           TEXT NOT NULL,
+  time_stamp        TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  parent_message_id INTEGER REFERENCES messages(id) ON DELETE CASCADE,
+  anonymous         BOOLEAN NOT NULL DEFAULT true
+);
+
+INSERT into users (username,password,email,first_name,member,admin) values('tman2747','','triston@gmail.com','triston',true,true);
+SELECT* from users;
+rollback;
