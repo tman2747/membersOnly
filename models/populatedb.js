da36fdec8647f19bc59f714e10386a0df61c7e05
Triston Land <tristonland@gmail.com>
2025-11-14 00:02:04 -0700

    add sql populate schema

--- models/populatedb.js
+++ models/populatedb.js
@@ -1,12 +1,33 @@
 // const { Client } = require("pg");
 
-// const SQL = `CREATE TABLE IF NOT EXISTS users (
-//     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-//     username varchar(255),
-//     post text,
-//     posted timestamp DEFAULT now()
-// );
-// `;
+const SQL = `BEGIN;
+
+CREATE TABLE IF NOT EXISTS users (
+  id          SERIAL PRIMARY KEY,
+  username    VARCHAR(36) UNIQUE NOT NULL,
+  password    VARCHAR(120) NOT NULL,
+  email       VARCHAR(320) UNIQUE,
+  first_name  VARCHAR(36) NOT NULL,
+  last_name   VARCHAR(36),
+  member      BOOLEAN NOT NULL DEFAULT false,
+  admin       BOOLEAN NOT NULL DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  id                SERIAL PRIMARY KEY,
+  author_id         INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  profile_id        INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  title             VARCHAR(280),
+  message           TEXT NOT NULL,
+  time_stamp        TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  parent_message_id INTEGER REFERENCES messages(id) ON DELETE CASCADE,
+  anonymous         BOOLEAN NOT NULL DEFAULT true
+);
+
+INSERT into users (username,password,email,first_name,member,admin) values('tman2747','','triston@gmail.com','triston',true,true);
+SELECT* from users;
+rollback;
+`;
 
 // async function main() {
 //   console.log(process.env.HOST);
@@ -27,4 +48,3 @@
 //   console.log("DONE");
 // }
 // main();
-
