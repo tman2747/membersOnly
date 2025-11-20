758e759c1a127574d407b0bbdeaa9317ccf90132
Triston Land <tristonland@gmail.com>
2025-11-20 02:22:36 -0700

    fix replies to questions. working :)

--- models/queries.js
+++ models/queries.js
@@ -18,10 +18,34 @@ exports.getByUsername = async (username) => {
 };
 exports.getProfileMessagesByID = async (id) => {
   const { rows } = await pool.query(
-    "SELECT messages.id as message_id, messages.author_id, users.username as author, messages.message, messages.profile_id,profile_user.username as profile_user,messages.time_stamp,messages.anonymous from messages join users on messages.author_id = users.id join users as profile_user on profile_user.id=messages.profile_id where messages.profile_id=$1;",
+    "SELECT messages.parent_message_id,messages.id as message_id, messages.author_id, users.username as author, messages.message, messages.profile_id,profile_user.username as profile_user,messages.time_stamp,messages.anonymous from messages join users on messages.author_id = users.id join users as profile_user on profile_user.id=messages.profile_id where messages.profile_id=$1;",
     [id]
   );
-  return rows;
+  console.log(rows);
+  const messages = new Map();
+
+  const rootMessages = [];
+  // make a map of the message data
+  rows.forEach((message) => {
+    messages.set(message.message_id, { ...message, replies: [] });
+  });
+  // go to the key of each input and assign it to the parent
+  // also added it to rootmessages for easier return
+  rows.forEach((message) => {
+    const currentMessage = messages.get(message.message_id);
+
+    if (currentMessage.parent_message_id == null) {
+      rootMessages.push(currentMessage);
+    } else {
+      const parentMessage = messages.get(currentMessage.parent_message_id);
+      if (parentMessage) {
+        parentMessage.replies.push(currentMessage);
+      }
+    }
+  });
+  console.log("messages: ", messages);
+  console.log("root messages: ", rootMessages);
+  return rootMessages;
 };
 exports.addMessageToProfile = async (authorId, message, profile_id) => {
   await pool.query(
